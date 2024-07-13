f946f2a3eae1c26229a79ee9dc714ff9f6bacfa4
VishwavedKelkar <138898035+VishwavedKelkar@users.noreply.github.com>
2024-07-13 23:26:20 +0530

    add 21st code

---
+++ prime_factors_of_a_number/src/test/java/com/prime_factors_of_a_number/prime_factors_of_a_number/PrimeFactorsOfANumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prime_factors_of_a_number.prime_factors_of_a_number;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeFactorsOfANumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
